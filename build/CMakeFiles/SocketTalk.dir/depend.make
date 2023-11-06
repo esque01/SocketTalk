4ddc9118310ea1ee9513b40cc970a611a89064a1
Edward Esqueda <esquede@outlook.com>
2023-11-05 21:37:21 -0500

    Removed folders

--- build/CMakeFiles/SocketTalk.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SocketTalk.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/edward/Development/C/SocketTalk
-# Build directory: /home/edward/Development/C/SocketTalk/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
